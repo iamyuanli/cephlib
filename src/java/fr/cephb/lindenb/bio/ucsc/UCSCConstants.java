1ff6af038745faf50d342531bd64999d704bcdad
plindenbaum.ceph <plindenbaum.ceph@fdc2b1e0-8002-11dd-aace-ddc5e87cd236>
2008-10-01 21:33:56 +0000

    generating POJO with velocity for UCSC

---
+++ src/java/fr/cephb/lindenb/bio/ucsc/UCSCConstants.java
@@ -0,0 +1,20 @@
+package fr.cephb.lindenb.bio.ucsc;
+
+import fr.cephb.lindenb.sql.MySQLConstants;
+
+/**
+ * UCSCConstants
+ * for connecting the UCSC anonymous mysql server
+ *
+ */
+public class UCSCConstants extends MySQLConstants
+{
+/** default database */
+public static final String DATABASE="hg18";
+/** default host */
+public static final String HOST="genome-mysql.cse.ucsc.edu";
+/** default user */
+public static final String USER="genome";
+/** default password */
+public static final String PASSWORD="";
+}
